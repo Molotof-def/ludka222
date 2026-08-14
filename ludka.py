7fd454d7fb1f912612cac0ff2dd96e2e39b30b49
Molotof-def <ivanivankov351@gmail.com>
2026-08-14 18:14:06 +0400

    Add files via upload

--- ludka.py
+++ ludka.py
@@ -395,5 +395,5 @@ async def main():
     print(f"✅ Бот запущен. Администраторов: {len(ADMIN_IDS)}")
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
